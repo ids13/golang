1a8ae4217ba846e40d06c35b5c5ef7bd478735e5
ids13 <idrusyuliyono@gmail.com>
2024-05-06 18:28:39 +0700

    embed struct di dasar

--- dasar/struct.go
+++ dasar/struct.go
@@ -9,6 +9,16 @@ type customer struct {
 	umur          int
 }
 
+type orang struct {
+	nama string
+	umur int
+}
+
+type murid struct {
+	orang
+	kelas int
+}
+
 func main() {
 	var data customer
 	data.nama = "naga"
@@ -26,4 +36,27 @@ func main() {
 
 	data3 := customer{"andi", "kuliah", 20}
 	fmt.Println(data3)
+	//embed struct
+	var data4 murid
+	data4.kelas = 12
+	data4.nama
+	data4.umur = 17
+	fmt.Println(data4.kelas)
+	fmt.Println(data4.nama)
+	fmt.Println(data4.umur)
+	/*
+		Go menyediakan akses langsung ke bidang-bidang yang ditanamkan
+		tanpa perlu menyebutkan nama struct yang ditanamkan.
+		Ini karena Go memperkenalkan mekanisme yang disebut "promotion" (peningkatan) untuk bidang-bidang yang ditanamkan.
+		Promotion berarti bahwa bidang-bidang dari struct yang ditanamkan secara otomatis "naik" ke struct yang menanamkannya.
+		Ketika Anda mengakses bidang-bidang yang ditanamkan,
+		Go akan mencari bidang tersebut terlebih dahulu di struct yang menanamkannya.
+		Jika bidang tersebut tidak ditemukan di struct yang menanamkannya,
+		Go akan mencari di struct yang ditanamkan.
+		Dalam contoh Anda, ketika Anda menulis data4.umur, Go akan mencari bidang umur di dalam struct murid.
+		Karena bidang umur tidak ditemukan di dalam struct murid,
+		Go akan melihat ke dalam struct yang ditanamkan, yaitu orang, dan menemukan bidang umur di sana.
+		Oleh karena itu, Anda bisa langsung menulis data4.umur
+		untuk mengakses bidang umur tanpa perlu menuliskan data4.orang.umur.
+	*/
 }
